915f9a70f379629303e9f46b798bffc10a5bf375
LouisaPawusch <louisa.pawusch@googlemail.com>
2025-08-13 13:14:56 -0400

    updated test and model

--- src/HydroInverseML/HIL_model.py
+++ src/HydroInverseML/HIL_model.py
@@ -7,6 +7,7 @@ class HIL_model(torch.nn.Module):
     def __init__(self, in_channel, filter_sizes, max_pool_kernel_sizes, dropout_rate, out_channels = None, stride = 1, batch_norm_momentum = 0.1, paddings=None, seed=0):
         super().__init__()
         self.num_hidden_layers = len(filter_sizes)
+        print(f"Number of hidden layers: {self.num_hidden_layers}")
         self.conv_layers = nn.ModuleList()
 
         assert self.num_hidden_layers == len(out_channels) if out_channels is not None else True, "Number of out_channels must match number of filter sizes."
@@ -14,19 +15,22 @@ class HIL_model(torch.nn.Module):
         # Encoder layers
         current_channel = in_channel
         pooling_counter = 0
-        for current_layer in range((self.num_hidden_layers+1) // 2):
+        for current_layer in range((self.num_hidden_layers + 1) // 2):
             out_channel = current_channel * 2 if out_channels is None else out_channels[current_layer]
             current_padding = ((filter_sizes[current_layer] - 1) // 2) if paddings is None else paddings[current_layer]
             self.conv_layers.append(nn.Conv2d(current_channel, out_channel, kernel_size=(filter_sizes[current_layer], filter_sizes[current_layer]), padding=current_padding, stride=stride, padding_mode='zeros'))
-            self.conv_layers.append(nn.ReLU())
+            self.conv_layers.append(nn.ELU())
             self.conv_layers.append(nn.BatchNorm2d(out_channel, momentum=batch_norm_momentum))
             if (current_layer + 1) % 2 == 0:
+                print(f"Adding MaxPool layer with kernel size {max_pool_kernel_sizes[pooling_counter]} at layer {current_layer}")
                 #self.conv_layers.append(nn.Dropout(dropout_rate))
-                self.conv_layers.append(nn.MaxPool2d(kernel_size=(max_pool_kernel_sizes[pooling_counter],max_pool_kernel_sizes[pooling_counter]), stride=max_pool_kernel_sizes[pooling_counter], padding=max_pool_kernel_sizes[pooling_counter] // 2))
+                self.conv_layers.append(nn.MaxPool2d(kernel_size=(max_pool_kernel_sizes[pooling_counter],max_pool_kernel_sizes[pooling_counter]), stride=max_pool_kernel_sizes[pooling_counter], padding=max_pool_kernel_sizes[pooling_counter] // 2, return_indices=True))
                 pooling_counter += 1
             current_channel = out_channel
  
+        print(f"Total pooling layers added: {pooling_counter}, expected: {len(max_pool_kernel_sizes)}")
         assert pooling_counter == len(max_pool_kernel_sizes), "Number of max pool kernel sizes must match number of executed pooling layers."
+        pooling_counter -= 1  # Adjust for the last pooling layer
 
         # Decoder layers
         for current_layer in range(((self.num_hidden_layers+1) // 2), self.num_hidden_layers):
@@ -38,7 +42,7 @@ class HIL_model(torch.nn.Module):
             out_channel = current_channel // 2 if out_channels is None else out_channels[current_layer]
             current_padding = ((filter_sizes[current_layer] - 1) // 2) if paddings is None else paddings[current_layer]
             self.conv_layers.append(nn.Conv2d(current_channel, out_channel, kernel_size=(filter_sizes[current_layer], filter_sizes[current_layer]), padding=current_padding,  stride=stride, padding_mode='zeros'))
-            self.conv_layers.append(nn.ReLU())
+            self.conv_layers.append(nn.ELU())
             self.conv_layers.append(nn.BatchNorm2d(out_channel, momentum=batch_norm_momentum))
             current_channel = out_channel
 
@@ -46,7 +50,7 @@ class HIL_model(torch.nn.Module):
         self.conv_layers.append(nn.ReLU())
 
     def forward(self, x):
-        pooling_counter = 0
+        pooling_counter = -1 # Start from -1 to handle the first pooling layer correctly
         before_pooling_shapes = []
         pooling_indices = []
         clones_for_skip_connections = []
--- testing/test_initial.py
+++ testing/test_initial.py
@@ -1,17 +1,26 @@
 import torch
 from HydroInverseML.src.HydroInverseML import HIL_model
 
+# to execute and show print statements run pytest -s
+
 def test_math():
     assert 1 + 1 == 2
 
 def test_HIL_model():
-    test_out_channels = [4, 16, 32]
-    test_filter_sizes = [5, 33, 9]
+    test_in_channel = 4
+    test_out_channels = [16, 32, 32, 16, 4]
+    test_filter_sizes = [5, 33, 9, 33, 5]
     test_max_pool_kernel_sizes = [2]
-    model = HIL_model(in_channel=4, filter_sizes=test_filter_sizes, max_pool_kernel_sizes=test_max_pool_kernel_sizes, dropout_rate=0.0, out_channels=test_out_channels, stride=1)
+    model = HIL_model(in_channel=test_in_channel, filter_sizes=test_filter_sizes, max_pool_kernel_sizes=test_max_pool_kernel_sizes, dropout_rate=0.0, out_channels=test_out_channels, stride=1)
     assert len(test_out_channels) == model.num_hidden_layers
-    # x = torch.randn((1, 4, 150, 150))
-    # y = model.conv_1(x)
-    # assert y.shape == (1, 16, 150, 150)
+    assert len(test_filter_sizes) == model.num_hidden_layers
+    print(f"Model created with {model.num_hidden_layers} hidden layers.")
+    print(model)
+
+    x = torch.randn((1, test_in_channel, 150, 150))
+    y = model(x)
+    print(f"Input shape: {x.shape}, Output shape: {y.shape}")
+    assert y.shape == (1, test_out_channels[-1], 150, 150)
+
 
     
